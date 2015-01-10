5ee44265ed3704f2f68d35c58b02e5bec9dee746
Jordan Harband <ljharb@gmail.com>
2015-01-10 11:15:25 -0800

    Moving the ES5 methods to their own internal module.

---
+++ es5.js
@@ -0,0 +1,115 @@
+'use strict';
+
+var toStr = Object.prototype.toString;
+
+var $isNaN = Number.isNaN || function (a) { return a !== a; };
+var $isFinite = Number.isFinite || function (x) { return typeof x === 'number' && !$isNaN(x) && x !== Infinity && x !== -Infinity; };
+
+var sign = function (number) {
+	return number >= 0 ? 1 : -1;
+};
+var mod = function (number, modulo) {
+	var remain = number % modulo;
+	return Math.floor(remain >= 0 ? remain : remain + modulo);
+};
+
+var isPrimitive = function (value) {
+	return value === null || typeof value !== 'object';
+};
+
+var IsCallable = function IsCallable(fn) {
+	// some older engines say that typeof /abc/ === 'function'
+	return typeof fn === 'function' && toStr.call(fn) === '[object Function]';
+};
+
+// https://es5.github.io/#x8.12
+var ES5internalSlots = {
+	'[[DefaultValue]]': function (O, hint) {
+		if (!hint) {
+			hint = toStr.call(O) === '[object Date]' ? String : Number;
+		}
+
+		if (hint === String || hint === Number) {
+			var methods = hint === String ? ['toString', 'valueOf'] : ['valueOf', 'toString'];
+			var value, i;
+			for (i = 0; i < methods.length; ++i) {
+				if (IsCallable(O[methods[i]])) {
+					value = O[methods[i]]();
+					if (isPrimitive(value)) {
+						return value;
+					}
+				}
+			}
+			throw new TypeError('No default value');
+		}
+		throw new TypeError('invalid [[DefaultValue]] hint supplied');
+	}
+};
+
+// https://es5.github.io/#x9
+var ES5 = {
+	ToPrimitive: function ToPrimitive(input, PreferredType) {
+		if (isPrimitive(input)) {
+			return input;
+		}
+		if (arguments.length < 2) {
+			PreferredType = toStr.call(input) === '[object Date]' ? String : Number;
+		}
+		if (PreferredType === String) {
+			return String(input);
+		} else if (PreferredType === Number) {
+			return Number(input);
+		} else {
+			throw new TypeError('invalid PreferredType supplied');
+		}
+		return ES5internalSlots['[[DefaultValue]]'](input, PreferredType);
+	},
+	ToBoolean: function ToBoolean(value) {
+		return Boolean(value);
+	},
+	ToNumber: function ToNumber(value) {
+		return Number(value);
+	},
+	ToInteger: function ToInteger(value) {
+		var number = this.ToNumber(value);
+		if ($isNaN(number)) { return 0; }
+		if (number === 0 || !$isFinite(number)) { return number; }
+		return sign(number) * Math.floor(Math.abs(number));
+	},
+	ToInt32: function ToInt32(x) {
+		return this.ToNumber(x) >> 0;
+	},
+	ToUint32: function ToUint32(x) {
+		return this.ToNumber(x) >>> 0;
+	},
+	ToUint16: function ToUint16(value) {
+		var number = this.ToNumber(value);
+		if ($isNaN(number) || number === 0 || !$isFinite(number)) { return 0; }
+		var posInt = sign(number) * Math.floor(Math.abs(number));
+		return mod(posInt, 0x10000);
+	},
+	ToString: function ToString(value) {
+		return String(value);
+	},
+	ToObject: function ToObject(value) {
+		this.CheckObjectCoercible(value);
+		return Object(value);
+	},
+	CheckObjectCoercible: function CheckObjectCoercible(value, optMessage) {
+		/* jshint eqnull:true */
+		if (value == null) {
+			throw new TypeError(optMessage || 'Cannot call method on ' + value);
+		}
+	},
+	IsCallable: IsCallable,
+	SameValue: function SameValue(x, y) {
+		if (x === y) {
+			// 0 === -0, but they are not identical.
+			if (x === 0) { return 1 / x === 1 / y; }
+			return true;
+		}
+        return $isNaN(x) && $isNaN(y);
+	}
+};
+
+module.exports = ES5;
