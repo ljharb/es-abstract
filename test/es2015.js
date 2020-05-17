5322bdef67de6d179d09900422fe95a14485ba30
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-05-17 14:50:00 +0200

    [Test]: Run tests with `undefined` this

--- test/es2015.js
+++ test/es2015.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var ES = require('../').ES2015;
+var boundES = require('./helpers/createBoundESNamespace')(ES);
 
 var ops = require('../operations/2015');
 
 var expectedMissing = ['Construct', 'CreateArrayFromList', 'CreateListIterator', 'NormalCompletion', 'RegExpBuiltinExec'];
 
-require('./tests').es2015(ES, ops, expectedMissing);
+require('./tests').es2015(boundES, ops, expectedMissing);
 
 require('./helpers/runManifestTest')(require('tape'), ES, 2015);
