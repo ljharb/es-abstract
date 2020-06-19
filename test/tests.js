72588995f79ba68fcd7753fb38ef566183daece0
Jordan Harband <ljharb@gmail.com>
2020-06-18 23:03:02 -0700

    [New] `ES2017`+: add `IsSharedArrayBuffer`, `OrdinaryToPrimitive`

--- test/tests.js
+++ test/tests.js
@@ -4637,6 +4637,100 @@ var es2017 = function ES2017(ES, ops, expectedMissing, skips) {
 		t.end();
 	});
 
+	test('OrdinaryToPrimitive', function (t) {
+		forEach(v.primitives, function (primitive) {
+			t['throws'](
+				function () { ES.OrdinaryToPrimitive(primitive, 'string'); },
+				TypeError,
+				debug(primitive) + ' is not Object'
+			);
+
+			if (primitive != null) {
+				t.equal(
+					ES.OrdinaryToPrimitive(Object(primitive), 'number'),
+					primitive,
+					debug(Object(primitive)) + ' becomes ' + debug(primitive)
+				);
+			}
+		});
+
+		t.test('Symbols', { skip: !v.hasSymbols }, function (st) {
+			forEach(v.symbols, function (sym) {
+				st.equal(
+					ES.OrdinaryToPrimitive(Object(sym), 'string'),
+					Symbol.prototype.toString.call(sym),
+					debug(Object(sym)) + ' with hint "string" returns ' + debug(Symbol.prototype.toString.call(sym))
+				);
+				st.equal(
+					ES.OrdinaryToPrimitive(Object(sym), 'number'),
+					sym,
+					debug(Object(sym)) + ' with hint "number" returns ' + debug(sym)
+				);
+			});
+
+			var primitiveSym = Symbol('primitiveSym');
+			var objectSym = Object(primitiveSym);
+			st.equal(
+				ES.OrdinaryToPrimitive(objectSym, 'string'),
+				Symbol.prototype.toString.call(primitiveSym),
+				debug(objectSym) + ' with hint "string" returns ' + debug(Symbol.prototype.toString.call(primitiveSym))
+			);
+			st.equal(
+				ES.OrdinaryToPrimitive(objectSym, 'number'),
+				primitiveSym,
+				debug(objectSym) + ' with hint "number" returns ' + debug(primitiveSym)
+			);
+			st.end();
+		});
+
+		t.test('Arrays', function (st) {
+			var arrays = [[], ['a', 'b'], [1, 2]];
+			forEach(arrays, function (arr) {
+				st.equal(ES.OrdinaryToPrimitive(arr, 'string'), String(arr), debug(arr) + ' with hint "string" returns the string version of the array');
+				st.equal(ES.OrdinaryToPrimitive(arr, 'number'), String(arr), debug(arr) + ' with hint "number" returns the string version of the array');
+			});
+			st.end();
+		});
+
+		t.test('Dates', function (st) {
+			var dates = [new Date(), new Date(0), new Date(NaN)];
+			forEach(dates, function (date) {
+				st.equal(ES.OrdinaryToPrimitive(date, 'string'), String(date), debug(date) + ' with hint "string" returns the string version of the date');
+				st.equal(ES.OrdinaryToPrimitive(date, 'number'), Number(date), debug(date) + ' with hint "number" returns the number version of the date');
+			});
+			st.end();
+		});
+
+		t.test('Objects', function (st) {
+			st.equal(ES.OrdinaryToPrimitive(v.coercibleObject, 'number'), v.coercibleObject.valueOf(), 'coercibleObject with hint "number" coerces to valueOf');
+			st.equal(ES.OrdinaryToPrimitive(v.coercibleObject, 'string'), v.coercibleObject.toString(), 'coercibleObject with hint "string" coerces to non-stringified toString');
+
+			st.equal(ES.OrdinaryToPrimitive(v.coercibleFnObject, 'number'), v.coercibleFnObject.toString(), 'coercibleFnObject with hint "number" coerces to non-stringified toString');
+			st.equal(ES.OrdinaryToPrimitive(v.coercibleFnObject, 'string'), v.coercibleFnObject.toString(), 'coercibleFnObject with hint "string" coerces to non-stringified toString');
+
+			st.equal(ES.OrdinaryToPrimitive({}, 'number'), '[object Object]', '{} with hint "number" coerces to Object#toString');
+			st.equal(ES.OrdinaryToPrimitive({}, 'string'), '[object Object]', '{} with hint "string" coerces to Object#toString');
+
+			st.equal(ES.OrdinaryToPrimitive(v.toStringOnlyObject, 'number'), v.toStringOnlyObject.toString(), 'toStringOnlyObject with hint "number" returns non-stringified toString');
+			st.equal(ES.OrdinaryToPrimitive(v.toStringOnlyObject, 'string'), v.toStringOnlyObject.toString(), 'toStringOnlyObject with hint "string" returns non-stringified toString');
+
+			st.equal(ES.OrdinaryToPrimitive(v.valueOfOnlyObject, 'number'), v.valueOfOnlyObject.valueOf(), 'valueOfOnlyObject with hint "number" returns valueOf');
+			st.equal(ES.OrdinaryToPrimitive(v.valueOfOnlyObject, 'string'), v.valueOfOnlyObject.valueOf(), 'valueOfOnlyObject with hint "string" returns non-stringified valueOf');
+
+			st.test('exceptions', function (s2t) {
+				s2t['throws'](ES.OrdinaryToPrimitive.bind(null, v.uncoercibleObject, 'number'), TypeError, 'uncoercibleObject with hint "number" throws a TypeError');
+				s2t['throws'](ES.OrdinaryToPrimitive.bind(null, v.uncoercibleObject, 'string'), TypeError, 'uncoercibleObject with hint "string" throws a TypeError');
+
+				s2t['throws'](ES.OrdinaryToPrimitive.bind(null, v.uncoercibleFnObject, 'number'), TypeError, 'uncoercibleFnObject with hint "number" throws a TypeError');
+				s2t['throws'](ES.OrdinaryToPrimitive.bind(null, v.uncoercibleFnObject, 'string'), TypeError, 'uncoercibleFnObject with hint "string" throws a TypeError');
+				s2t.end();
+			});
+			st.end();
+		});
+
+		t.end();
+	});
+
 	test('StringGetOwnProperty', function (t) {
 		forEach(v.nonStrings.concat(v.strings), function (nonBoxedString) {
 			t['throws'](
@@ -4688,6 +4782,30 @@ var es2017 = function ES2017(ES, ops, expectedMissing, skips) {
 
 		t.equal(ES.ToIndex(3), 3, 'numbers work');
 		t.equal(ES.ToIndex(v.valueOfOnlyObject), 4, 'coercible objects are coerced');
+
+		t.end();
+	});
+
+	test('IsSharedArrayBuffer', function (t) {
+		forEach(v.primitives, function (primitive) {
+			t['throws'](
+				function () { ES.IsSharedArrayBuffer(primitive); },
+				TypeError,
+				debug(primitive) + ' is not an Object'
+			);
+		});
+
+		forEach(v.objects, function (nonSAB) {
+			t.equal(ES.IsSharedArrayBuffer(nonSAB), false, debug(nonSAB) + ' is not a SharedArrayBuffer');
+		});
+		/* globals SharedArrayBuffer: false */
+
+		t.test('real SABs', { skip: typeof SharedArrayBuffer !== 'function' }, function (st) {
+			var sab = new SharedArrayBuffer();
+			st.equal(ES.IsSharedArrayBuffer(sab), true, debug(sab) + ' is a SharedArrayBuffer');
+
+			st.end();
+		});
 
 		t.end();
 	});
