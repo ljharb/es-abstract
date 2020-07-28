97439946a55bad88eac1709a7ae6c3d57d32baa1
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-07-28 22:50:00 +0200

    [Fix]: `helpers/callBound`: Avoid useless `$indexOf` call

--- helpers/callBound.js
+++ helpers/callBound.js
@@ -1,14 +1,11 @@
 'use strict';
 
 var GetIntrinsic = require('../GetIntrinsic');
-
 var callBind = require('./callBind');
-
-var $indexOf = callBind(GetIntrinsic('String.prototype.indexOf'));
 
 module.exports = function callBoundIntrinsic(name, allowMissing) {
 	var intrinsic = GetIntrinsic(name, !!allowMissing);
-	if (typeof intrinsic === 'function' && $indexOf(name, '.prototype.')) {
+	if (typeof intrinsic === 'function') {
 		return callBind(intrinsic);
 	}
 	return intrinsic;
