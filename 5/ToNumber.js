2cf08d8bf88214e8f0e9b85ac06952a4632188e6
Jordan Harband <ljharb@gmail.com>
2021-03-01 20:00:22 -0800

    [Fix] `ES5`: `ToNumber`: properly handle signed hex literals

--- 5/ToNumber.js
+++ 5/ToNumber.js
@@ -12,7 +12,7 @@ module.exports = function ToNumber(value) {
 
 	// eslint-disable-next-line no-control-regex
 	var trimmed = prim.replace(/^[ \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u0085]+|[ \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u0085]+$/g, '');
-	if ((/^0[ob]/).test(trimmed)) {
+	if ((/^0[ob]|^[+-]0x/).test(trimmed)) {
 		return NaN;
 	}
 
