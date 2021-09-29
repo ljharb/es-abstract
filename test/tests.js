7669a1bbe70c6fb486d6ff0b9f99fbb2ba9af9d3
Richard Gibson <richard.gibson@gmail.com>
2021-09-29 14:35:32 -0400

    [Tests] Correct the behavior of `safeBigInt`

--- test/tests.js
+++ test/tests.js
@@ -36,8 +36,18 @@ if (hasBigInts) {
 	}
 }
 var safeBigInt = needsBigIntHack ? function (int) {
-	if (int > MAX_SAFE_INTEGER) {
-		return eval(int + 'n'); // eslint-disable-line no-eval
+	if (int > MAX_SAFE_INTEGER || -int > MAX_SAFE_INTEGER) {
+		// construct a maximum-precision string of digits
+		// from a native serialization like <digits>.<zeroes> or <digit>.<digits>e+<exponent>
+		var preciseParts = Number(int).toPrecision(100).split('e');
+		var significand = preciseParts[0].replace(/(\.[0-9]*?)0*$/, '$1');
+		var baseTenExponent = Number(preciseParts[1] || 0);
+		if (baseTenExponent > 0) {
+			var significandScale = (significand + '.').indexOf('.');
+			baseTenExponent -= significand.length - 1 - significandScale;
+		}
+		var digits = significand.replace('.', '') + Array(baseTenExponent + 1).join('0');
+		return eval(digits + 'n'); // eslint-disable-line no-eval
 	}
 	return $BigInt(int);
 } : $BigInt;
@@ -7443,8 +7453,7 @@ var es2020 = function ES2020(ES, ops, expectedMissing, skips) {
 		t.end();
 	});
 
-	// TODO: fix these tests/this AO in node v10.4-v10.8, and remove `needsBigIntHack`
-	test('ToBigInt64', { skip: !hasBigInts || needsBigIntHack }, function (t) {
+	test('ToBigInt64', { skip: !hasBigInts }, function (t) {
 		var twoSixtyThreeMinusOne = twoSixtyThree - BigInt(1);
 		var negTwoSixtyThreeMinusOne = -twoSixtyThree - BigInt(1);
 
@@ -7465,8 +7474,7 @@ var es2020 = function ES2020(ES, ops, expectedMissing, skips) {
 		t.end();
 	});
 
-	// TODO: fix these tests/this AO in node v10.4-v10.8, and remove `needsBigIntHack`
-	test('ToBigUint64', { skip: !hasBigInts || needsBigIntHack }, function (t) {
+	test('ToBigUint64', { skip: !hasBigInts }, function (t) {
 		var twoSixtyFourMinusOne = twoSixtyFour - BigInt(1);
 		var twoSixtyThreeMinusOne = twoSixtyThree - BigInt(1);
 		var negTwoSixtyThreeMinusOne = -twoSixtyThree - BigInt(1);
