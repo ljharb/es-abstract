d108356f4df3378f69c1699b87571cd1f43b61d4
Jordan Harband <ljharb@gmail.com>
2022-04-08 14:45:25 -0700

    [Robustness] use `exec` instead of `test`, since the latter observably looks up `exec`

--- helpers/regexTester.js
+++ helpers/regexTester.js
@@ -1,11 +1,9 @@
 'use strict';
 
-var GetIntrinsic = require('get-intrinsic');
+var callBound = require('call-bind/callBound');
 
-var $test = GetIntrinsic('RegExp.prototype.test');
-
-var callBind = require('call-bind');
+var $exec = callBound('RegExp.prototype.exec');
 
 module.exports = function regexTester(regex) {
-	return callBind($test, regex);
+	return function test(s) { return $exec(regex, s) !== null; };
 };
