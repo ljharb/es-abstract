1e9cda50e3a02efde4ec49ce0e716a091761e22e
Jordan Harband <ljharb@gmail.com>
2023-02-27 19:00:58 -0800

    [Fix] `ES2015`+: `ToUint8Clamp`: avoid an extra observable ToNumber

--- 2016/ToUint8Clamp.js
+++ 2016/ToUint8Clamp.js
@@ -11,7 +11,7 @@ module.exports = function ToUint8Clamp(argument) {
 	var number = ToNumber(argument);
 	if ($isNaN(number) || number <= 0) { return 0; }
 	if (number >= 0xFF) { return 0xFF; }
-	var f = floor(argument);
+	var f = floor(number);
 	if (f + 0.5 < number) { return f + 1; }
 	if (number < f + 0.5) { return f; }
 	if (f % 2 !== 0) { return f + 1; }
