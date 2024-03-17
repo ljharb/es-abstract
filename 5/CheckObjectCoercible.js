4080de871d70de64d57844853cd2f5cd01a497ec
Jordan Harband <ljharb@gmail.com>
2024-03-17 00:18:19 -0700

    [Fix] `ES5`: `CheckObjectCoercible`: use the right function name

--- 5/CheckObjectCoercible.js
+++ 5/CheckObjectCoercible.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var RequireObjectCoercible = require('es-object-atoms/RequireObjectCoercible');
+
 // http://262.ecma-international.org/5.1/#sec-9.10
 
-module.exports = require('es-object-atoms/RequireObjectCoercible');
+module.exports = function CheckObjectCoercible(value) {
+	return RequireObjectCoercible(value);
+};
