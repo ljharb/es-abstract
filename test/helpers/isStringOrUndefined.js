8f742b632bc2ac2f397683b939b9d7f588474c26
Jordan Harband <ljharb@gmail.com>
2024-11-25 17:47:53 -1000

    [Tests] `isStringOrUndefined`: increase coverage

--- test/helpers/isStringOrUndefined.js
+++ test/helpers/isStringOrUndefined.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var test = require('tape');
+var v = require('es-value-fixtures');
+var inspect = require('object-inspect');
+
+var forEach = require('../../helpers/forEach');
 
 var isStringOrUndefined = require('../../helpers/isStringOrUndefined');
 
 test('isStringOrUndefined', function (t) {
-	t.ok(isStringOrUndefined('foo', 0, ['foo', 'bar', 'baz']), 'string item is a string or undefined');
+	t.ok(isStringOrUndefined('foo'), 'string is a string or undefined');
 
-	t.ok(isStringOrUndefined(undefined, 0, [undefined, 'bar', 'baz']), 'present undefined item is a string or undefined');
+	t.ok(isStringOrUndefined(undefined), 'undefined is a string or undefined');
+
+	forEach(v.primitives, function (primitive) {
+		if (typeof primitive !== 'undefined' && typeof primitive !== 'string') {
+			t.notOk(isStringOrUndefined(primitive), inspect(primitive) + ' is not a string or undefined');
+		}
+	});
 
 	t.end();
 });
