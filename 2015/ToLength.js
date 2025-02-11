be4db7ee9a44788a4a6edf167922a9c0dbf40cd3
Jordan Harband <ljharb@gmail.com>
2025-02-11 14:12:40 -0800

    [meta] add missing comments

--- 2015/ToLength.js
+++ 2015/ToLength.js
@@ -4,6 +4,8 @@ var MAX_SAFE_INTEGER = require('math-intrinsics/constants/maxSafeInteger');
 
 var ToInteger = require('./ToInteger');
 
+// https://262.ecma-international.org/6.0/#sec-tolength
+
 module.exports = function ToLength(argument) {
 	var len = ToInteger(argument);
 	if (len <= 0) { return 0; } // includes converting -0 to +0
--- 2022/ToLength.js
+++ 2022/ToLength.js
@@ -4,6 +4,8 @@ var MAX_SAFE_INTEGER = require('math-intrinsics/constants/maxSafeInteger');
 
 var ToIntegerOrInfinity = require('./ToIntegerOrInfinity');
 
+// https://262.ecma-international.org/12.0/#sec-tolength
+
 module.exports = function ToLength(argument) {
 	var len = ToIntegerOrInfinity(argument);
 	if (len <= 0) { return 0; } // includes converting -0 to +0
