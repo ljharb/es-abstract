42d9e8cf8978c4c73acf8f22d50eb60c1060efe7
Jordan Harband <ljharb@gmail.com>
2026-04-22 13:36:36 -0700

    [Refactor] `ES2020`+: `modulo`: support BigInt inputs

--- 2022/modulo.js
+++ 2022/modulo.js
@@ -2,8 +2,16 @@
 
 var mod = require('../helpers/mod');
 
-// https://262.ecma-international.org/5.1/#sec-5.2
+// https://262.ecma-international.org/11.0/#eqn-modulo
 
 module.exports = function modulo(x, y) {
+	if (typeof x === 'bigint') {
+		if (typeof y !== 'bigint') {
+			throw new TypeError('`x` and `y` must both be BigInts or both be Numbers');
+		}
+		var r = x % y;
+		if (r === BigInt(0)) { return r; }
+		return (r < BigInt(0)) === (y < BigInt(0)) ? r : r + y;
+	}
 	return mod(x, y);
 };
--- test/methods/modulo.js
+++ test/methods/modulo.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var esV = require('../helpers/v');
+
 module.exports = function (t, year, modulo) {
 	t.ok(year >= 5, 'ES5+');
 
@@ -8,4 +10,30 @@ module.exports = function (t, year, modulo) {
 
 	t.equal(-3 % 2, -1, '-3 % 2 is -1');
 	t.equal(modulo(-3, 2), 1, '-3 mod 2 is +1');
+
+	if (year >= 2020) {
+		t.test('BigInts', { skip: !esV.hasBigInts }, function (st) {
+			st.equal(modulo(BigInt(3), BigInt(2)), BigInt(1), '+3n mod +2n is +1n');
+			st.equal(modulo(BigInt(-3), BigInt(2)), BigInt(1), '-3n mod +2n is +1n');
+			st.equal(modulo(BigInt(3), BigInt(-2)), BigInt(-1), '+3n mod -2n is -1n');
+			st.equal(modulo(BigInt(-3), BigInt(-2)), BigInt(-1), '-3n mod -2n is -1n');
+
+			st.equal(modulo(BigInt(6), BigInt(3)), BigInt(0), '+6n mod +3n is 0n');
+			st.equal(modulo(BigInt(0), BigInt(3)), BigInt(0), '0n mod +3n is 0n');
+			st.equal(modulo(BigInt(0), BigInt(-3)), BigInt(0), '0n mod -3n is 0n');
+
+			st['throws'](
+				function () { modulo(BigInt(1), 2); },
+				TypeError,
+				'mixing bigint and number throws'
+			);
+			st['throws'](
+				function () { modulo(1, BigInt(2)); },
+				TypeError,
+				'mixing number and bigint throws'
+			);
+
+			st.end();
+		});
+	}
 };
