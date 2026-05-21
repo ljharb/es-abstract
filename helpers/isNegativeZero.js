aad41037adfc53afc182956aff80c4d5b88bba3b
Jordan Harband <ljharb@gmail.com>
2026-05-21 13:05:05 +0200

    [Tests] coverage-ignore TBD helpers

--- helpers/isNegativeZero.js
+++ helpers/isNegativeZero.js
@@ -1,6 +1,4 @@
 'use strict';
 
 // TODO, semver-major: remove
-module.exports = function isNegativeZero(x) {
-	return x === 0 && 1 / x === 1 / -0;
-};
+module.exports = require('math-intrinsics/isNegativeZero');
